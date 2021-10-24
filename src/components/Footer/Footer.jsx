98f103d0ae69c496fd6920a1742a7a310ef44830
Liliana Vasquez <lilitakerloki@gmail.com>
2021-10-24 15:39:09 -0500

    Add font, colors, logo and icons in svg format

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import iconFacebook from "../../assets/img/iconFacebook.svg"
+import iconLinkedin from "../../assets/img/iconLinkedin.svg"
+import iconTwitter from "../../assets/img/iconTwitter.svg"
+import iconInstagram from "../../assets/img/iconInstagram.svg"
 
 class Footer extends React.Component{
 
@@ -6,19 +10,12 @@ class Footer extends React.Component{
 
         return(
             <footer>
-                <small className="copyright-text">©2021, Digital Rental Car</small>
+                <small className="copyright-text">©2021, Digital Booking</small>
                 <div className="footer-icons">
-                    <a href="#">
-                        <i className="fab fa-facebook fab-icon"></i></a>
-                    <a href="#">
-                        <i className="fab fa-linkedin-in fab-icon"></i>
-                    </a>
-                    <a href="#">
-                        <i className="fab fa-twitter fab-icon"></i>
-                    </a>
-                    <a href="#">
-                        <i className="fab fa-instagram"></i>
-                    </a>
+                    <a href="#" className="footer-icons__padding-right"><img src={iconFacebook} alt="Ícono de Facebook" /></a>
+                    <a href="#" className="footer-icons__padding-right"><img src={iconLinkedin} alt="Ícono de Linkedin" /></a>
+                    <a href="#" className="footer-icons__padding-right"><img src={iconTwitter} alt="Ícono de Twitter" /></a>
+                    <a href="#"><img src={iconInstagram} alt="Ícono de Instagram" /></a>
                 </div>
             </footer>
         )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../../general.css";
+import logoWeb from "../../assets/img/logo.svg";
+import iconMenu from "../../assets/img/iconMenu-Mobile.svg";
 
 class Header extends React.Component {
 
@@ -9,13 +11,13 @@ class Header extends React.Component {
             <header>
                 <div className="header-links">
                     <div className="header-links__logo">
-                        <a href="./index.html" title="Link a la página principal"><img alt="Logo de Digital Rental Car" src="#"/></a>
+                        <a href="./index.html" title="Link a la página principal"><img alt="Logo de Digital Booking" src={logoWeb}/></a>
                     </div>
                     <div className="header-links__slogan">
                         <a href="./index.html" title="Link a la página principal"><p>Los mejores carros a tu disposición</p></a> 
                     </div>
                 </div>
-                <i className="fas fa-bars"></i>
+                <img className="menu-icon" src={iconMenu} alt="Ícono menú"/>
                 <div className="header-input">
                     <input type="submit" value="Crear cuenta" className="header-input__signup"/>
                     <input type="submit" value="Iniciar sesión"/>
