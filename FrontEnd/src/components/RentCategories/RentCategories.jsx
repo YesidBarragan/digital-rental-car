b91f13e994c4cd630cce155c8cb16f8c5efee4d4
Liliana Vasquez <lilitakerloki@gmail.com>
2021-10-27 11:45:00 -0500

    Fix Front End files

--- FrontEnd/src/components/RentCategories/RentCategories.jsx
+++ FrontEnd/src/components/RentCategories/RentCategories.jsx
@@ -3,18 +3,21 @@ import "./RentCategories.css";
 
 function RentCategories(props) {
     return (
-      <div>
-        <h1>BUSCAR POR TIPO DE ALQUILER </h1>
-        <ul>
+      <div className="rentCategories"> 
+        <h2>BUSCAR POR TIPO DE ALQUILER </h2>
+        <div className="line">
           {props.typesOfRent.map((typeOfRent) => (
-            <li>
-              <img src={typeOfRent.imgUrl} alt=""/>
-              <h2>{typeOfRent.descripcion}</h2>
-              <p>{typeOfRent.cantidad}</p>
-            </li>
+            <div className="card-header">
+              <img  src={typeOfRent.imgUrl}  alt={typeOfRent.descripcion} className="category-rent-img"/>
+              <div className="textos">
+                <h3 className="category-name">{typeOfRent.descripcion}</h3>
+                <p>{typeOfRent.cantidad}</p>
+              </div>
+            </div>
           ))}
-        </ul>
+        </div>
       </div>
+
     );
   }
   
