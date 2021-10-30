22913fbb71ae23a0ab622256ed7e89b54a51320d
YesidBarragan <yesarmand2006@gmail.com>
2021-10-29 19:57:12 -0500

    Fix FrontEnd files

--- FrontEnd/src/components/RentCategories/RentCategories.jsx
+++ FrontEnd/src/components/RentCategories/RentCategories.jsx
@@ -7,7 +7,7 @@ function RentCategories(props) {
         <h2>BUSCAR POR TIPO DE ALQUILER </h2>
         <div className="line">
           {props.typesOfRent.map((typeOfRent) => (
-            <div className="card-header">
+            <div key={typeOfRent.id} className="card-header">
               <img  src={typeOfRent.imgUrl}  alt={typeOfRent.descripcion} className="category-rent-img"/>
               <div className="textos">
                 <h3 className="category-name">{typeOfRent.descripcion}</h3>
--- FrontEnd/src/components/SearchingBlock/SearchingBlock.jsx
+++ FrontEnd/src/components/SearchingBlock/SearchingBlock.jsx
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 /* import { Link } from "react-router-dom"; */
+import DatePicker from "react-datepicker";
+import { registerLocale, setDefaultLocale } from  "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import es from 'date-fns/locale/es';
 import "./SearchingBlock.css";
+import carsData from "../../assets/data/cars.json"
+setDefaultLocale('es');
+registerLocale('es', es)
 
-class SearchingBlock extends React.Component {
+export const SearchingBlock = () => {
 
-    render(){
+    const [dateRange, setDateRange] = useState([null, null]);
+    const [startDate, endDate] = dateRange;
 
-        return(
+    const [searchCityOpen, setSearchCityOpen] = useState(false);
+    let cityInputRef = useRef();
+    let cityOptionRef = useRef();
 
-            <div className="main-box">
-                <h1>Busca ofertas en todo tipo de carros</h1>
-                <form>
-                    <input className="form-input__map" type="text" placeholder="¿A dónde vamos?"/>
-                    <input className="form-input__check" type="text" placeholder="Check in - Check out"/>
-                    <button>Buscar</button>
-                </form>
-            </div>
-
-        )
-
+    const handleToggle = () => {
+        setSearchCityOpen(!searchCityOpen);
     }
 
-}
+    const handleSelect = () => {
+        setSearchCityOpen(!searchCityOpen);
+        cityInputRef.current.value = cityOptionRef.current.value;
+    }
 
-export default SearchingBlock;
+    return (
+
+        <div className="main-box">
+            <h1>Busca ofertas en todo tipo de carros</h1>
+            <form>
+                <input className="form-input__map" type="text" placeholder="¿A dónde vamos?" onClick={handleToggle} ref={cityInputRef}/>
+                { searchCityOpen && (<div className="search-city">
+                    { carsData.map(carData => (
+                        // <div key={carData.id} className="card-car">
+                            <option key={carData.id} onClick={handleSelect} ref={cityOptionRef}>{carData.location}</option>
+                        // </div>
+                        )) }
+                </div>) }
+                {/* <input className="form-input__check" type="text" placeholder="Check in - Check out"/> */}
+                <DatePicker 
+                    className="form-input__check date-picker"
+                    placeholderText="Check in - Check out"
+                    dateFormat="dd/MM/yyyy"
+                    selectsRange={true}
+                    startDate={startDate}
+                    endDate={endDate}
+                    onChange={(update) => {
+                        setDateRange(update);
+                    }}
+                />
+                <button className="search-button">Buscar</button>
+            </form>
+        </div>
+    )
+}
